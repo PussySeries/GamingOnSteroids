55c6014025ad31fbf17a8bdacee064adc6724dbe
PussySeries <67498923+PussySeries@users.noreply.github.com>
2020-08-09 11:34:31 +0200

    Add files via upload

---
+++ Champions/PussySeriesAio/newVersion.lua
@@ -0,0 +1,19 @@
+Data = {
+    Loader = {
+        Version = 0.01,
+    },
+    
+	Core = {
+        Changelog = "Auto Version ChangeLog Here",
+        Version = 0.01,
+    },
+
+    Champions = {
+        Yone = {
+            Changelog = "Yone Changelog Here",
+            Version = 0.01,
+        },
+    },
+}
+
+return Data
