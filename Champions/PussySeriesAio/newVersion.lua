c3e78046855e01eab22cd9d43037144f948548a4
PussySeries <67498923+PussySeries@users.noreply.github.com>
2020-08-10 14:09:58 +0200

    Update newVersion.lua

--- Champions/PussySeriesAio/newVersion.lua
+++ Champions/PussySeriesAio/newVersion.lua
@@ -5,13 +5,13 @@ Data = {
     
 	Core = {
         Changelog = "Auto Version ChangeLog Here",
-        Version = 0.01,
+        Version = 0.02,
     },
 
     Champions = {
         Yone = {
             Changelog = "Yone Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },
     },
 }
